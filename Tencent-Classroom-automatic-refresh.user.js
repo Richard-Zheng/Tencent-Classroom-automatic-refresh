5d638211ff1aed6209b8e0427b484dd062d9edab
Richard-Zheng <yuqiu36@live.com>
2020-04-21 19:09:59 +0800

    fix some bugs

--- Tencent-Classroom-automatic-refresh.user.js
+++ Tencent-Classroom-automatic-refresh.user.js
@@ -15,49 +15,52 @@ var lastpage = false;
 
 (function() {
     'use strict';
-    window.onload = function() {
-        //wait for load complete
-        setTimeout(() => {
-            //get buttons
-            var prev = document.getElementsByClassName("tab-move-btn tab-prev-btn");
-            var next = document.getElementsByClassName("tab-move-btn tab-next-btn");
-            
-            //check for first/last page
-            if (prev.length == 0) {
-                this.firstpage = true;
-            } else if (next.length == 0) {
-                this.lastpage = true;
-            }
+    //wait for load complete
+    setTimeout(() => {
+        //create checkbox
+        var oCheckbox=document.createElement("input");
+        var myText=document.createTextNode("启用自动刷新");
+        oCheckbox.setAttribute("type","checkbox");
+        oCheckbox.setAttribute("id","enable-auto-reload");
+        if (GM_getValue('enableplugin')  == null) {
+            GM_setValue('enableplugin', false);  //init value
+        }
+        oCheckbox.checked = GM_getValue('enableplugin');
+        oCheckbox.onclick = function setEnable() {
+            GM_setValue('enableplugin', oCheckbox.checked);
 
-            //create checkbox
-            var oCheckbox=document.createElement("input");
-            var myText=document.createTextNode("启用自动刷新");
-            oCheckbox.setAttribute("type","checkbox");
-            oCheckbox.setAttribute("id","enable-auto-reload");
-            if (GM_getValue('enableplugin')  == null) {
-                GM_setValue('enableplugin', false);
+            if (oCheckbox.checked == true) {
+                location.reload();
             }
-            oCheckbox.checked = GM_getValue('enableplugin');
-            oCheckbox.onclick = function setEnable() {
-                GM_setValue('enableplugin', oCheckbox.checked);
-            }
-            var mydiv=document.getElementsByClassName("header-index-search")[0];
-            mydiv.appendChild(oCheckbox);
-            mydiv.appendChild(myText);
+        }
+        var mydiv=document.getElementsByClassName("header-index-search")[0];
+        mydiv.appendChild(oCheckbox);
+        mydiv.appendChild(myText);
 
-            //auto click next
-            if (GM_getValue('enableplugin')) {
-                if (!this.lastpage) {
+        //auto click next
+        if (GM_getValue('enableplugin')) {
+            function toNext() {
+                //get buttons
+                var prev = document.getElementsByClassName("tab-move-btn tab-prev-btn");
+                var next = document.getElementsByClassName("tab-move-btn tab-next-btn");
+
+                //check for first/last page
+                if (prev.length == 0) {
+                    firstpage = true;
+                } else if (next.length == 0) {
+                    lastpage = true;
+                }
+
+                if (!lastpage && GM_getValue('enableplugin')) {
                     next[0].click();
-                    setTimeout(() => { //wait for load
-                        this.location.reload();
+                    setTimeout(() => {
+                        toNext();  //loop the function
                     }, 1000);
-                } else {
-                    alert("done");
                 }
             }
-        }, 500);
-        
-    }
+
+            toNext();
+        }
+    }, 1000);
     
 })();
