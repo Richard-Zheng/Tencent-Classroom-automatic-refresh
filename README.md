48624c7acb34050a6d3f0d466d04a6a424c21334
Richard-Zheng <yuqiu36@live.com>
2020-04-21 17:07:21 +0800

    Version 0.1

---
+++ README.md
@@ -0,0 +1,15 @@
+# Tencent Classroom - automatic refresh
+
+A [tampermonkey](https://www.tampermonkey.net/) script for automatically refreshing the "curriculum" page of Tencent Classroom
+
+## How to use
+
+1. Use a modern web browser(ie Chrome or Firefox)
+2. Install tampermonkey plugin and enable it
+3. Open the [raw JavaScript file](https://github.com/Richard-Zheng/Tencent-Classroom-automatic-refresh/raw/master/Tencent-Classroom-automatic-refresh.user.js), then Click "Install"
+4. Refresh the web page
+
+## Coming soon
+
+- Notice when a teacher starts class
+- Automatically enter the classroom
---
+++ Tencent-Classroom-automatic-refresh.user.js
@@ -0,0 +1,63 @@
+// ==UserScript==
+// @name         Tencent Classroom - automatic refresh
+// @name:zh-CN   腾讯课堂 - 课程表页自动刷新
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Automatically refresh the "Curriculum" page
+// @description:zh-cn  自动刷新“课程表”页面
+// @author       Richard-Zheng
+// @match        https://ke.qq.com/user/index/*
+// @grant   GM_getValue
+// @grant   GM_setValue
+// ==/UserScript==
+var firstpage = false;
+var lastpage = false;
+
+(function() {
+    'use strict';
+    window.onload = function() {
+        //wait for load complete
+        setTimeout(() => {
+            //get buttons
+            var prev = document.getElementsByClassName("tab-move-btn tab-prev-btn");
+            var next = document.getElementsByClassName("tab-move-btn tab-next-btn");
+            
+            //check for first/last page
+            if (prev.length == 0) {
+                this.firstpage = true;
+            } else if (next.length == 0) {
+                this.lastpage = true;
+            }
+
+            //create checkbox
+            var oCheckbox=document.createElement("input");
+            var myText=document.createTextNode("启用自动刷新");
+            oCheckbox.setAttribute("type","checkbox");
+            oCheckbox.setAttribute("id","enable-auto-reload");
+            if (GM_getValue('enableplugin')  == null) {
+                GM_setValue('enableplugin', false);
+            }
+            oCheckbox.checked = GM_getValue('enableplugin');
+            oCheckbox.onclick = function setEnable() {
+                GM_setValue('enableplugin', oCheckbox.checked);
+            }
+            var mydiv=document.getElementsByClassName("header-index-search")[0];
+            mydiv.appendChild(oCheckbox);
+            mydiv.appendChild(myText);
+
+            //auto click next
+            if (GM_getValue('enableplugin')) {
+                if (!this.lastpage) {
+                    next[0].click();
+                    setTimeout(() => { //wait for load
+                        this.location.reload();
+                    }, 1000);
+                } else {
+                    alert("done");
+                }
+            }
+        }, 500);
+        
+    }
+    
+})();
